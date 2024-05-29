c900fe79b81d032586bd74415234aaca6ff31da9
135791maria <mjosedossantosmarques92@gmail.com>
2024-05-29 10:58:03 -0300

    slide-css

---
+++ scripts.js
@@ -0,0 +1,34 @@
+
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide-item');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) {
+            slide.classList.add('active');
+        }
+    });
+
+}
+
+
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    showSlide(currentSlide);
+});
+
+
+
+
